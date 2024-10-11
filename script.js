e34287c86cd6117d74588301aa8f1a7bd905cb7d
Vickysingh314 <Innocenthevickyboy99@gmail.com>
2024-10-11 17:08:17 +0530

    "updated"

--- script.js
+++ script.js
@@ -221,3 +221,22 @@ function startFallingElements() {
     }
 }
 
+
+
+
+
+
+
+// Fullscreen function for video
+function openFullscreen() {
+    const video = document.getElementById('birthdayVideo');
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) { // Firefox
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) { // IE/Edge
+        video.msRequestFullscreen();
+    }
+}
